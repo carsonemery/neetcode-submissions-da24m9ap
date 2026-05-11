6d0445be46135f2757bed41fefcceeaf5beacac0
Carson Emery <76604247+carsonemery@users.noreply.github.com>
2026-05-10 20:41:35 -0600

    Add: path-sum - submission-3

---
+++ Data Structures & Algorithms/path-sum/submission-3.rs
@@ -0,0 +1,25 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//     pub val: i32,
+//     pub left: Option<Rc<RefCell<TreeNode>>>,
+//     pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+impl Solution {
+    pub fn has_path_sum(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32) -> bool {
+        return Self::dfs(&root, 0, target_sum);
+    }
+
+    fn dfs(node: &Option<Rc<RefCell<TreeNode>>>, cur_sum: i32, target: i32) -> bool {
+        if let Some(n) = node {
+            let n = n.borrow();
+            let new_sum = cur_sum + n.val;
+            if n.left.is_none() && n.right.is_none() {
+                return new_sum == target;
+            }
+            Self::dfs(&n.left, new_sum, target) || Self::dfs(&n.right, new_sum, target)
+        } else {
+            false
+        }
+    }
+}
